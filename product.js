63c92ce9e038274c43bb9c15e902df7225999909
ajecoreglobalservicesltd-netizen <ajecoreglobalservicesltd@gmail.com>
2026-08-16 03:15:03 +0100

    product.js

--- product.js
+++ product.js
@@ -1,4 +1,4 @@
-mport {supabase,esc,money,user} from "./supabase.js";
+import {supabase,esc,money,user} from "./supabase.js";
 
 const root = document.querySelector("#product");
 const id = new URLSearchParams(location.search).get("id");
