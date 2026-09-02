0a17fe23917fae9b7e42cc8d9e856a05d317a0e0
ajecoreglobalservicesltd-netizen <ajecoreglobalservicesltd@gmail.com>
2026-09-02 18:57:40 +0100

    Delete config.js

--- config.js
+++
@@ -1,3 +0,0 @@
-// PUBLIC browser configuration only. Never put a Supabase service_role/secret key here.
-export const SUPABASE_URL="https://zeyoecwbndiocsaqlnuk.supabase.co";
-export const SUPABASE_KEY="sb_publishable_2QQNc64ugY41jjlvCcsnqw_bFHyQsyz";
